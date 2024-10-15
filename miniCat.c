9520024bc33abd5700e5ec5ba4c6d2a1d95b8106
Lordabgrund <57839843+Lordabgrund@users.noreply.github.com>
2024-10-15 17:20:19 +0200

    add miniCat

---
+++ miniCat.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+   // char filename[200];
+    FILE *fp = fopen(argv[1], "r");
+
+    if (fp == NULL)
+    {
+        printf("%s %s %s", "Error: could not open file", argv[1], "\n");
+        return 1;
+    } else {
+    // read one character at a time and
+    // display it to the output
+    char ch;
+    while ((ch = fgetc(fp)) != EOF)
+        putchar(ch);
+
+    // close the file
+    fclose(fp);
+    }
+    return 0;
+}
